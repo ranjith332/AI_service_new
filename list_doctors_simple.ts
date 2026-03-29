f2dbabf506d004e4b4ca1b5b7cc0638bc357df82
ranjith332 <ranjith.a@edmondremedis.com>
2026-03-29 23:52:00 +0530

    conflict problems

--- list_doctors_simple.ts
+++
@@ -1,6 +0,0 @@
-import { DatabaseClient } from "./src/db/client";
-
-const db = new DatabaseClient();
-const res = await db.query({ text: "SELECT id, first_name, last_name, tenant_id FROM doctors", values: [], description: "list_doctors" });
-console.log(JSON.stringify(res.rows, null, 2));
-await db.close();
